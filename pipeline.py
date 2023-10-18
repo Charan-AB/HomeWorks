f1f1a23d7a3e644de0d14bda61875d46e67cfd74
Charan-AB <89255446+Charan-AB@users.noreply.github.com>
2023-10-18 00:07:03 -0600

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,1 @@
-import sys 
-import pandas as pd
 
-if (len(sys.argv) != 3):
-    print('enter two arguments');
-    sys.exit(1);
-
-arg1 = sys.argv[1];
-arg2 = sys.argv[2];
-
-df = pd.read_csv(arg1);
-
-for i in range(len(df)):
-    df.loc[i,'Cloud'] = float("{:.2f}".format(df.loc[i,'Cloud']));
-
-df.to_csv(arg2, index = False);
